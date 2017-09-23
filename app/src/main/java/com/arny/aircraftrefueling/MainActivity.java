8c88dff96bcce770a6bf353dd5436d71c81b9a92
arny <arnyigor@gmail.com>
2017-09-23 22:58:35 +0300

    refactring

--- app/src/main/java/com/arny/aircraftrefueling/MainActivity.java
+++ app/src/main/java/com/arny/aircraftrefueling/MainActivity.java
@@ -1,36 +1,24 @@
 package com.arny.aircraftrefueling;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
-import com.arny.aircraftrefueling.adapters.TabPagerAdapter;
 import com.arny.aircraftrefueling.fragments.KilogrammFragment;
 import com.arny.aircraftrefueling.fragments.LitreFragment;
-import com.arny.arnylib.utils.ToastMaker;
 import com.mikepenz.google_material_typeface_library.GoogleMaterial;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-	private static final int MENU_KILO = 0;
-	private static final int MENU_LITRE = 1;
+	private static final int MENU_FUEL = 0;
+	private static final int MENU_DEICE = 1;
 	private static final String DRAWER_SELECTION = "drawer_selection";
 	private Context context = MainActivity.this;
 	private Drawer drawer = null;
@@ -48,29 +36,8 @@ public class MainActivity extends AppCompatActivity {
 			getSupportActionBar().setLogo(R.mipmap.ic_launcher);
 			getSupportActionBar().setDisplayUseLogoEnabled(true);
 		}
-		toolbar.setTitle(getString(R.string.app_name));
 		initDrawer();
-		restoreDraverPosition(savedInstanceState);
-	}
-
-	private void restoreDraverPosition(Bundle savedInstanceState) {
-		Log.i(MainActivity.class.getSimpleName(), "restoreDraverPosition: savedInstanceState = " + savedInstanceState);
-		if (savedInstanceState == null) {
-			drawer.setSelection(MENU_KILO);
-		} else {
-			try {
-				drawer.setSelection(Long.parseLong(savedInstanceState.getString(DRAWER_SELECTION)));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		outState = drawer.saveInstanceState(outState);
-		outState.putString(DRAWER_SELECTION, String.valueOf(drawer.getCurrentSelection()));
-		super.onSaveInstanceState(outState);
+		selectItem(MENU_FUEL);
 	}
 
 	private void initDrawer() {
@@ -82,13 +49,13 @@ public class MainActivity extends AppCompatActivity {
 				.withActionBarDrawerToggleAnimated(true)
 				.addDrawerItems(
 						new PrimaryDrawerItem()
-								.withIdentifier(MENU_KILO)
-								.withName(R.string.menu_kilogramms)
-								.withIcon(GoogleMaterial.Icon.gmd_format_color_fill),
+								.withIdentifier(MENU_FUEL)
+								.withName(R.string.menu_fueling)
+								.withIcon(getResources().getDrawable(R.drawable.ic_fuel)),
 						new PrimaryDrawerItem()
-								.withIdentifier(MENU_LITRE)
-								.withName(R.string.menu_litres)
-								.withIcon(GoogleMaterial.Icon.gmd_opacity)
+								.withIdentifier(MENU_DEICE)
+								.withName(R.string.menu_deicing)
+								.withIcon(getResources().getDrawable(R.drawable.ic_snowflake))
 				)
 				.withOnDrawerItemClickListener((view, position, drawerItem) -> {
 					selectItem((int) drawerItem.getIdentifier());
@@ -98,8 +65,7 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	private void selectItem(int position) {
-		Fragment fragment = getFragment(position);
-		commitFragment(fragment);
+		commitFragment(getFragment(position));
 	}
 
 	private void commitFragment(Fragment fragment) {
@@ -114,13 +80,13 @@ public class MainActivity extends AppCompatActivity {
 	private Fragment getFragment(int position) {
 		Fragment fragment = null;
 		switch (position) {
-			case MENU_KILO:
+			case MENU_FUEL:
 				fragment = new KilogrammFragment();
-				toolbar.setTitle(getString(R.string.menu_kilogramms));
+				toolbar.setTitle(getString(R.string.menu_fueling));
 				break;
-			case MENU_LITRE:
+			case MENU_DEICE:
 				fragment = new LitreFragment();
-				toolbar.setTitle(getString(R.string.menu_litres));
+				toolbar.setTitle(getString(R.string.menu_deicing));
 				break;
 		}
 		return fragment;
--- app/src/main/java/com/arny/aircraftrefueling/fragments/KilogrammFragment.java
+++ app/src/main/java/com/arny/aircraftrefueling/fragments/KilogrammFragment.java
@@ -1,13 +1,11 @@
 package com.arny.aircraftrefueling.fragments;
 
-
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -18,202 +16,223 @@ import android.widget.Toast;
 
 import com.arny.aircraftrefueling.R;
 import com.arny.aircraftrefueling.Utils;
+import com.arny.arnylib.files.FileUtils;
 import com.arny.arnylib.utils.DateTimeUtils;
 import com.arny.arnylib.utils.DroidUtils;
+import com.arny.arnylib.utils.ToastMaker;
 import com.arny.arnylib.utils.Utility;
 
+import java.io.File;
 import java.util.Locale;
 
-public class KilogrammFragment extends Fragment {
-    private static final double LITRE_AM_GALLON = 0.2642;
-    private static final String DIR_SD = "AirRefuelFiles";
-    private static final String FILENAME_SD = "AirRefuelKilo.txt";
-    private final double WING_TANK_MAX_LITRE = 4876.0;
-    private final double NO_USE_LITRE = 50;
-    private EditText edtDensityFuel, edtRequiredfuel, edtTotalFuel;
-    private String left, right, centre, FileText, FileTextNull;
-    private double mMassOstat, mMassRequired, mReqMassTotal, mRo;
-    private TextView tvCT, tvLT, tvRT, tvTotalKilo, tvTotalLitre, tvReqHeader;
-    private static final int V_UNIT_LITRE = 0;
-    private static final int V_UNIT_AM_GAL = 1;
-    private String v_unit;
-    private int vUnit;
-    private Context context;
-    private Button btnKiloFileSave,btnKiloFileDel;
+public class KilogrammFragment extends Fragment implements View.OnClickListener {
+	private static final double LITRE_AM_GALLON = 0.2642;
+	private static final String DIR_SD = "AirRefuelFiles";
+	private static final String FILENAME_SD = "AirRefuelKilo.txt";
+	private final double WING_TANK_MAX_LITRE = 4876.0;
+	private final double NO_USE_LITRE = 50;
+	private EditText edtDensityFuel, edtRequiredfuel, edtTotalFuel;
+	private String left, right, centre;
+	private double mMassOstat, mMassRequired, mReqMassTotal, mRo;
+	private TextView tvCT, tvLT, tvRT, tvTotalKilo, tvTotalLitre, tvReqHeader;
+	private static final int V_UNIT_LITRE = 0;
+	private static final int V_UNIT_AM_GAL = 1;
+	private String v_unit;
+	private int vUnit;
+	private Context context;
+	private Button btnKiloFileSave, btnKiloFileDel;
+	private Button btnCalc;
 
-    public KilogrammFragment() {
-        // Required empty public constructor
-    }
+	public KilogrammFragment() {
+		// Required empty public constructor
+	}
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
-    }
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_kilogramm, container, false);
+		context = container.getContext();
+		edtTotalFuel = (EditText) rootView.findViewById(R.id.editTotalMass);
+		edtDensityFuel = (EditText) rootView.findViewById(R.id.editDensityFuel);
+		edtRequiredfuel = (EditText) rootView.findViewById(R.id.editRequiredMass);
+		tvTotalLitre = (TextView) rootView.findViewById(R.id.tvTotalLitre);
+		tvTotalKilo = (TextView) rootView.findViewById(R.id.tvTotalKilo);
+		tvLT = (TextView) rootView.findViewById(R.id.tvLT);
+		tvRT = (TextView) rootView.findViewById(R.id.tvRT);
+		tvCT = (TextView) rootView.findViewById(R.id.tvCT);
+		tvReqHeader = (TextView) rootView.findViewById(R.id.tvReqHeader);
+		final Spinner vUnits = (Spinner) rootView.findViewById(R.id.volumeUnit);
+		btnCalc = (Button) rootView.findViewById(R.id.buttonKiloCnt);
+		btnKiloFileSave = (Button) rootView.findViewById(R.id.btnKiloSave);
+		btnKiloFileDel = (Button) rootView.findViewById(R.id.btnKiloDel);
+		if (Utils.isFileExist(context, DIR_SD, FILENAME_SD)) {
+			btnKiloFileDel.setVisibility(View.VISIBLE);
+		}
+		btnKiloFileSave.setOnClickListener(this);
+		btnKiloFileDel.setOnClickListener(this);
+		btnCalc.setOnClickListener(this);
+		ArrayAdapter<String> vUnitsAdapter = new ArrayAdapter<>(context, R.layout.v_unit_item, getResources().getStringArray(R.array.volumeUnits));
+		vUnits.setAdapter(vUnitsAdapter);
+		vUnits.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+				v_unit = adapterView.getItemAtPosition(i).toString();
+				vUnit = i;
+				tvReqHeader.setText(v_unit);
+				tvTotalLitre.setText(null);
+			}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_kilogramm, container, false);
-        context = container.getContext();
-        edtTotalFuel = (EditText) rootView.findViewById(R.id.editTotalMass);
-        edtDensityFuel = (EditText) rootView.findViewById(R.id.editDensityFuel);
-        edtRequiredfuel = (EditText) rootView.findViewById(R.id.editRequiredMass);
-        tvTotalLitre = (TextView) rootView.findViewById(R.id.tvTotalLitre);
-        tvTotalKilo = (TextView) rootView.findViewById(R.id.tvTotalKilo);
-        tvLT = (TextView) rootView.findViewById(R.id.tvLT);
-        tvRT = (TextView) rootView.findViewById(R.id.tvRT);
-        tvCT = (TextView) rootView.findViewById(R.id.tvCT);
-        tvReqHeader = (TextView) rootView.findViewById(R.id.tvReqHeader);
-        final Spinner vUnits = (Spinner) rootView.findViewById(R.id.volumeUnit);
-        final Button buttonKiloCnt = (Button) rootView.findViewById(R.id.buttonKiloCnt);
-        btnKiloFileSave = (Button) rootView.findViewById(R.id.btnKiloSave);
-        btnKiloFileDel = (Button) rootView.findViewById(R.id.btnKiloDel);
-        if (Utils.isFileExist(context, DIR_SD, FILENAME_SD)) {
-            btnKiloFileDel.setVisibility(View.VISIBLE);
-        }
-        btnKiloFileSave.setOnClickListener(btnSavefileListener);
-        btnKiloFileDel.setOnClickListener(btnFileDelListener);
-        buttonKiloCnt.setOnClickListener(btnClick);
-        ArrayAdapter<String> vUnitsAdapter = new ArrayAdapter<>(context, R.layout.v_unit_item, getResources().getStringArray(R.array.volumeUnits));
-        vUnits.setAdapter(vUnitsAdapter);
-        vUnits.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                v_unit = adapterView.getItemAtPosition(i).toString();
-                vUnit = i;
-                tvReqHeader.setText(v_unit);
-                tvTotalLitre.setText(null);
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-                v_unit = adapterView.getItemAtPosition(V_UNIT_LITRE).toString();
-                vUnit = V_UNIT_LITRE;
-                tvReqHeader.setText(v_unit);
-                tvTotalLitre.setText(null);
-            }
-        });
-        return rootView;
-    }
-    View.OnClickListener btnFileDelListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-	        DroidUtils.alertConfirmDialog(context, getString(R.string.file_del), () -> {
-		        if (Utils.isFileExist(context,DIR_SD,FILENAME_SD)){
-			        if (Utils.deleteFileSD(context,DIR_SD,FILENAME_SD)){
-				        btnKiloFileDel.setVisibility(View.GONE);
-			        }
-		        }
-	        });
-        }
-    };
-    View.OnClickListener btnSavefileListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-	        DroidUtils.alertConfirmDialog(context, getString(R.string.file_save_data), () -> {
-		        getfileText();
-		        if (tvTotalLitre.getText().toString().trim().equals("")) {
-			        Toast.makeText(context, getString(R.string.error_calulate_requied), Toast.LENGTH_LONG).show();
-		        } else {
-			        if (!Utils.writeFileSD(context,DIR_SD,FILENAME_SD,FileText)){
-				        Toast.makeText(context, getString(R.string.error_file_not_write), Toast.LENGTH_SHORT).show();
-				        return;
-			        }
-		        }
-		        btnKiloFileDel.setVisibility(View.VISIBLE);
-	        });
-        }
-    };
-    View.OnClickListener btnClick = view -> calculateFuelCapacity();
+			@Override
+			public void onNothingSelected(AdapterView<?> adapterView) {
+				v_unit = adapterView.getItemAtPosition(V_UNIT_LITRE).toString();
+				vUnit = V_UNIT_LITRE;
+				tvReqHeader.setText(v_unit);
+				tvTotalLitre.setText(null);
+			}
+		});
+		return rootView;
+	}
 
-    private void getfileText() {
-        FileText = DateTimeUtils.getDateTime("dd MMM yyyy HH:mm:ss") + "\n" +
-                getString(R.string.file_fuel_remain) +
-		        Utils.getFString("%.0f", mMassOstat) +
-                getString(R.string.sh_unit_kilo) + ";" +
-                getString(R.string.file_fueled) +
-                Utils.getFString("%.0f", mReqMassTotal) +
-                getString(R.string.sh_unit_litre) + ";" +
-                getString(R.string.unit_density) + ": " +
-                Utils.getFString("%.3f", mRo) +
-                getString(R.string.sh_unit_kilo_on_litre) +
-                getString(R.string.file_total_litre) + "\n";
-    }
+	private String getFileText() {
+		return DateTimeUtils.getDateTime("dd MMM yyyy HH:mm:ss") + "\n" +
+				getString(R.string.file_fuel_remain) +
+				Utils.getFString("%.0f", mMassOstat) +
+				getString(R.string.sh_unit_kilo) + ";" +
+				getString(R.string.file_fueled) +
+				Utils.getFString("%.0f", mReqMassTotal) +
+				getString(R.string.sh_unit_litre) + ";" +
+				getString(R.string.unit_density) + ": " +
+				Utils.getFString("%.3f", mRo) +
+				getString(R.string.sh_unit_kilo_on_litre) +
+				getString(R.string.litre_qty) + tvTotalLitre.getText().toString() + "\n";
+	}
 
-    /**
-     * Функция заправки
-     *
-     * @param massreq сколько необходимо килограмм
-     * @param mro массовая плотность
-     */
-    public void tankFueling(double massreq, double mro) {
-        double maxCen = 2 * (WING_TANK_MAX_LITRE * mro - NO_USE_LITRE);
-        double l, r, c;
-        if (massreq <= maxCen) {
-            l = massreq / 2;
-            r = massreq / 2;
-            left = String.format("%.0f",l) ;
-            right = String.format("%.0f",r) ;
-            centre = String.format("%d",0) ;
-        }else{
-            c = massreq - maxCen;
-            l = massreq - c;
-            r = l / 2;
-            left = String.format("%.0f",r) ;
-            right = String.format("%.0f",r) ;
-            centre = String.format("%.0f",c) ;
-        }
-    }
+	public boolean isFileExist() {
+		if (!Environment.getExternalStorageState().equals("mounted")) {
+			Toast.makeText(context, ("SD-карта не доступна: " + Environment.getExternalStorageState()), Toast.LENGTH_LONG).show();
+			return false;
+		} else {
+			File file = new File(String.valueOf(Environment.getExternalStorageDirectory().getAbsolutePath()) + "/" + "AirRefuelFiles", "AirRefuelKilo.txt");
+			return !(!file.exists() || !file.isFile());
+		}
+	}
 
-    private void calculateFuelCapacity() {
-        if (Utility.empty(edtTotalFuel.getText().toString())) {
-            Toast.makeText(context, R.string.error_val_on_board, Toast.LENGTH_LONG).show();
-            return;
-        }
-        if (Utility.empty(edtRequiredfuel.getText().toString()) || edtRequiredfuel.getText().toString().trim().equals("0")) {
-            Toast.makeText(context, R.string.error_val_req, Toast.LENGTH_LONG).show();
-            return;
-        }
-        if (Utility.empty(edtDensityFuel.getText().toString()) || edtDensityFuel.getText().toString().trim().equals("0")) {
-            Toast.makeText(context, R.string.error_val_density, Toast.LENGTH_LONG).show();
-            return;
-        }
-        try {
-            mRo = Float.parseFloat(edtDensityFuel.getText().toString());
-            mMassOstat = Float.parseFloat(edtTotalFuel.getText().toString());
-            mMassRequired = Float.parseFloat(edtRequiredfuel.getText().toString());
-            String totStr = LitreCnt(mMassOstat, mRo, mMassRequired);
-            double totLit = (!totStr.equals("0") || !totStr.equals("")) ? Double.parseDouble(totStr) : 0;
-            if (totLit < 0) {
-                Toast.makeText(context, getString(R.string.error_wrong_data), Toast.LENGTH_SHORT).show();
-                return;
-            }
-            totLit = getTotLit(totLit);
-            tvTotalLitre.setText(String.format(Locale.getDefault(), "%.0f", totLit));
-            tvTotalKilo.setText(String.format(Locale.getDefault(), "%.0f", mReqMassTotal));
-            tankFueling(mMassRequired, mRo);
-            tvLT.setText(String.format("%s:\n%s", getString(R.string.left_fuel_tank), left));
-            tvRT.setText(String.format("%s:\n%s", getString(R.string.right_fuel_tank), right));
-            tvCT.setText(String.format("%s:\n%s", getString(R.string.center_fuel_tank), centre));
-            btnKiloFileSave.setVisibility(View.VISIBLE);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	/**
+	 * Функция заправки
+	 *
+	 * @param massreq сколько необходимо килограмм
+	 * @param mro     массовая плотность
+	 */
+	public void tankFueling(double massreq, double mro) {
+		double maxCen = 2 * (WING_TANK_MAX_LITRE * mro - NO_USE_LITRE);
+		double l, r, c;
+		if (massreq <= maxCen) {
+			l = massreq / 2;
+			r = massreq / 2;
+			left = String.format("%.0f", l);
+			right = String.format("%.0f", r);
+			centre = String.format("%d", 0);
+		} else {
+			c = massreq - maxCen;
+			l = massreq - c;
+			r = l / 2;
+			left = String.format("%.0f", r);
+			right = String.format("%.0f", r);
+			centre = String.format("%.0f", c);
+		}
+	}
 
-    private double getTotLit(double totLit) {
-        switch (vUnit) {
-            case V_UNIT_AM_GAL:
-                totLit = totLit * LITRE_AM_GALLON;
-                break;
-            default:
-                break;
-        }
-        return totLit;
-    }
+	private void calculateFuelCapacity() {
+		if (Utility.empty(edtTotalFuel.getText().toString())) {
+			Utils.toast(context,getString(R.string.error_val_on_board), false);
+			return;
+		}
+		if (Utility.empty(edtRequiredfuel.getText().toString()) || edtRequiredfuel.getText().toString().trim().equals("0")) {
+			Utils.toast(context,getString(R.string.error_val_req), false);
+			return;
+		}
+		if (Utility.empty(edtDensityFuel.getText().toString()) || edtDensityFuel.getText().toString().trim().equals("0")) {
+			Utils.toast(context,getString(R.string.error_val_density), false);
+			return;
+		}
+		try {
+			mRo = Float.parseFloat(edtDensityFuel.getText().toString());
+			mMassOstat = Float.parseFloat(edtTotalFuel.getText().toString());
+			mMassRequired = Float.parseFloat(edtRequiredfuel.getText().toString());
+			String totStr = LitreCnt(mMassOstat, mRo, mMassRequired);
+			double totLit = (!totStr.equals("0") || !totStr.equals("")) ? Double.parseDouble(totStr) : 0;
+			if (totLit < 0) {
+				Utils.toast(context,getString(R.string.error_wrong_data), false);
+				return;
+			}
+			totLit = getTotLit(totLit);
+			tvTotalLitre.setText(String.format(Locale.getDefault(), "%.0f", totLit));
+			tvTotalKilo.setText(String.format(Locale.getDefault(), "%.0f", mReqMassTotal));
+			tankFueling(mMassRequired, mRo);
+			tvLT.setText(String.format("%s:\n%s", getString(R.string.left_fuel_tank), left));
+			tvRT.setText(String.format("%s:\n%s", getString(R.string.right_fuel_tank), right));
+			tvCT.setText(String.format("%s:\n%s", getString(R.string.center_fuel_tank), centre));
+			btnKiloFileSave.setVisibility(View.VISIBLE);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    private String LitreCnt(double ostat, double mro, double mreq) {
-        mReqMassTotal = mreq - ostat;
-        return String.format("%.0f", (mReqMassTotal / mro));
-    }
+	private double getTotLit(double totLit) {
+		switch (vUnit) {
+			case V_UNIT_AM_GAL:
+				totLit = totLit * LITRE_AM_GALLON;
+				break;
+			default:
+				break;
+		}
+		return totLit;
+	}
 
+	private String LitreCnt(double ostat, double mro, double mreq) {
+		mReqMassTotal = mreq - ostat;
+		return String.format("%.0f", (mReqMassTotal / mro));
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.btnKiloDel:
+				DroidUtils.alertConfirmDialog(context, getString(R.string.file_del), () -> {
+					if (Utils.isFileExist(context, DIR_SD, FILENAME_SD)) {
+						Boolean delFile = Utils.deleteFileSD(context, DIR_SD, FILENAME_SD);
+						if (delFile) {
+							Utils.toast(context,getString(R.string.file_deleted), true);
+							btnKiloFileDel.setVisibility(View.GONE);
+						}
+					}
+				});
+				break;
+			case R.id.buttonKiloCnt:
+				calculateFuelCapacity();
+				break;
+			case R.id.btnKiloSave:
+				DroidUtils.alertConfirmDialog(context, getString(R.string.file_save_data), () -> {
+					if (tvTotalLitre.getText().toString().trim().equals("")) {
+						Utils.toast(context,getString(R.string.error_calulate_requied), false);
+					} else {
+						String pathWithName = FileUtils.getWorkDir(context) + "/" +DIR_SD + "/" + FILENAME_SD;
+						Boolean writeRes = FileUtils.writeToFile(context, pathWithName, getFileText());
+						if (writeRes) {
+							Utils.toast(context,getString(R.string.success_file_write)+":" + pathWithName, true);
+							if (Utils.isFileExist(context, DIR_SD, FILENAME_SD)) {
+									btnKiloFileDel.setVisibility(View.VISIBLE);
+							}
+						} else {
+							Utils.toast(context,getString(R.string.error_file_not_write), false);
+						}
+					}
+					if (Utils.isFileExist(context, DIR_SD, FILENAME_SD)) {
+						btnKiloFileDel.setVisibility(View.VISIBLE);
+					}else{
+						btnKiloFileDel.setVisibility(View.GONE);
+					}
+				});
+				break;
+		}
+	}
 }
